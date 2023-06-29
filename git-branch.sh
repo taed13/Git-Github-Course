61c8f3f9310218b523068b0ff84ace9e30484dae
taed13 <taedtech13@gmail.com>
2023-06-29 23:53:44 +0700

    updated index.html with emergency fix and create git-branch-merge.sh

--- git-branch.sh
+++ git-branch.sh
@@ -51,3 +51,15 @@ Untracked files:
         git-branch.sh
 
 no changes added to commit (use "git add" and/or "git commit -a")
+
+On branch emergency-fix
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+        modified:   index.html
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        git-branch.sh
+
+no changes added to commit (use "git add" and/or "git commit -a")
