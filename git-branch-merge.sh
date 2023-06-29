9c886be9b7e5a8dd0619e29c6ca400e1ee52d519
taed13 <taedtech13@gmail.com>
2023-06-29 23:58:55 +0700

    modified git-branch-merge

---
+++ git-branch-merge.sh
@@ -0,0 +1,13 @@
+git merge emergency-fix             
+
+Updating c6e4377..61c8f3f
+Fast-forward
+ git-branch-merge.sh |  0
+ git-branch.sh       | 65 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ index.html          |  1 +
+ 3 files changed, 66 insertions(+)
+ create mode 100644 git-branch-merge.sh
+ create mode 100644 git-branch.sh
+
+ git branch -d emergency-fix    
+ Deleted branch emergency-fix (was 61c8f3f).
